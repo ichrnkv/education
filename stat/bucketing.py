9639382405c2afcf105c79f3068419da188cd55d
Ilya Chernikov <45786833+ichrnkv@users.noreply.github.com>
2020-04-09 17:16:49 +0300

    Create bucketing.py

---
+++ stat/bucketing.py
@@ -0,0 +1,26 @@
+from scipy import stats
+import numpy as np
+import pandas as pd
+
+
+def bucketing(df, n=100):
+    """
+    Splits data into buckets
+    """
+    # buckets = np.zeros(n)
+    buckets = pd.DataFrame()
+    for i in range(int(df.shape[0]/n)):
+        sample = df.sample(n)
+        sample['cohort'] = i
+        buckets = pd.concat([buckets, sample])
+        df.drop(sample.index)
+    return buckets
+
+if __name__ == '__main__':
+    
+    # generate data
+    r = stats.expon.rvs(size=1000000)
+    df = pd.DataFrame(r, columns=['value'])
+    bucketed_df = bucketing(df, n=1000)
+    buckets = bucketed_df.groupby('cohort')['value'].agg(np.mean)
+    
